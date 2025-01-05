b798b794fc7bd2b4c547a87e9f1c611c86afc27f
paulo-win <xtbr295@gmail.com>
2025-01-04 23:48:59 -0300

    deploy api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ecogest-api.onrender.com/",
+  baseURL: "http://ecogestapp.sytes.net/",
   headers: {
     "Content-Type": "application/json",
   },
