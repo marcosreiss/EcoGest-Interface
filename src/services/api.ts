d4c64022b873c1bb118102e4eeb19e6f65d8a6b1
paulo-win <xtbr295@gmail.com>
2025-01-05 23:27:23 -0300

    revogar dotenv

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.API_URL, // Utilize o sistema de variáveis de ambiente do seu build tool
+  baseURL: "http:/ecogestapp.sytes.net:5000/", // Utilize o sistema de variáveis de ambiente do seu build tool
   headers: {
     "Content-Type": "application/json",
   },
