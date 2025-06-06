18907fdfcab09defaf8fc0ee6ee2c577b3e8dd92
Marcos Vinicius Reis <141747726+marcosreiss@users.noreply.github.com>
2025-06-06 15:24:46 -0300

    --add dar baixa em payable

--- src/hooks/usePayble.ts
+++ src/hooks/usePayble.ts
@@ -1,46 +1,50 @@
-import type { AxiosError } from "axios";
-import type { Payble, PaybleList, PayableParams } from "src/models/payable";
+import type { AxiosError } from 'axios';
+import type {
+  Payble,
+  PaybleList,
+  PayableParams,
+  UpdatePayableStatusParams,
+} from 'src/models/payable';
 
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 
 import {
   deletePaybleService,
   getPaybleByIdService,
   getPayblesPagedService,
-  updatePaybleStatusService,
+  updatePayableStatusService,
   updatePayabledataPagamentoService,
-} from "src/services/paybleService";
+} from 'src/services/paybleService';
 
 /**
  * Hook para obter uma lista paginada de pagáveis com filtros opcionais.
  */
 export const useGetPayblesPaged = (params: PayableParams) =>
   useQuery<PaybleList, AxiosError>({
-    queryKey: ["paybles-list", { params }],
+    queryKey: ['paybles-list', { params }],
     queryFn: () => getPayblesPagedService(params),
   });
-
 
 /**
  * Hook para obter os detalhes de um pagável pelo ID.
  */
 export const useGetPaybleById = (id: number) =>
   useQuery<Payble, AxiosError>({
-    queryKey: ["payble", id],
+    queryKey: ['payble', id],
     queryFn: () => getPaybleByIdService(id),
   });
 
-export const useUpdatePaybleStatus = () => {
+export const useUpdatePayableStatus = () => {
   const queryClient = useQueryClient();
 
-  return useMutation<number, AxiosError, { id: number}>({
-    mutationFn: ({ id }) => updatePaybleStatusService(id),
+  return useMutation<number, AxiosError, UpdatePayableStatusParams>({
+    mutationFn: (params) => updatePayableStatusService(params),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['paybles-list'] });
       queryClient.invalidateQueries({ queryKey: ["payble"] });
     },
     onError: (error) => {
-      console.error("Erro ao atualizar o status do pagável:", error);
+      console.error('Erro ao atualizar o status da conta a pagar:', error);
     },
   });
 };
@@ -48,19 +52,18 @@ export const useUpdatePaybleStatus = () => {
 export const useUpdateDataPagamentoPayable = () => {
   const queryClient = useQueryClient();
 
-  return useMutation< Payble, AxiosError, { dataPagamento: string; payableId: number }>({
+  return useMutation<Payble, AxiosError, { dataPagamento: string; payableId: number }>({
     mutationFn: ({ dataPagamento, payableId }) =>
       updatePayabledataPagamentoService(dataPagamento, payableId),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["paybles-list"] });
-      queryClient.invalidateQueries({ queryKey: ["payble"] });
+      queryClient.invalidateQueries({ queryKey: ['paybles-list'] });
+      queryClient.invalidateQueries({ queryKey: ['payble'] });
     },
     onError: (error) => {
-      console.error("Erro ao atualizar a data de pagamento da conta a pagar:", error);
+      console.error('Erro ao atualizar a data de pagamento da conta a pagar:', error);
     },
   });
 };
-
 
 /**
  * Hook para deletar um pagável.
@@ -71,9 +74,8 @@ export const useDeletePayble = () => {
     mutationFn: (id) => deletePaybleService(id),
     onSuccess: () => {
       queryClient.invalidateQueries({
-        queryKey: ["paybles-list"],
+        queryKey: ['paybles-list'],
       });
     },
   });
 };
-
--- src/services/paybleService.ts
+++ src/services/paybleService.ts
@@ -1,11 +1,10 @@
-import type { Payble, PaybleList, PayableParams } from "src/models/payable";
+import type { Payble, PaybleList, PayableParams, UpdatePayableStatusParams } from 'src/models/payable';
 
-import api from "./api";
-
+import api from './api';
 
 export const getPayblesPagedService = async (params: PayableParams): Promise<PaybleList> => {
   console.log(params);
-  const response = await api.get<PaybleList>("/payables", { params });
+  const response = await api.get<PaybleList>('/payables', { params });
   return response.data;
 };
 
@@ -14,16 +13,26 @@ export const getPaybleByIdService = async (id: number): Promise<Payble> => {
   return response.data;
 };
 
-export const updatePaybleStatusService = async (
-  paybleId: number,
+export const updatePayableStatusService = async (
+  params: UpdatePayableStatusParams
 ): Promise<number> => {
-  
-  const response = await api.put(`/payables/status?id=${paybleId}`);
+  const { id, status, date, payedValue } = params;
+
+  const query = new URLSearchParams();
+  query.append('id', id.toString());
+  query.append('status', status);
+  if (date) query.append('date', date);
+  if (payedValue !== undefined) query.append('payedValue', payedValue.toString());
+
+  const response = await api.put(`/payables/status?${query.toString()}`);
   return response.status;
 };
 
-export const updatePayabledataPagamentoService = async (dataPagamento: string, payableId: number): Promise<Payble> => {
-  const response = await api.put<Payble>(`/payables?id=${payableId}`, {dataPagamento});
+export const updatePayabledataPagamentoService = async (
+  dataPagamento: string,
+  payableId: number
+): Promise<Payble> => {
+  const response = await api.put<Payble>(`/payables?id=${payableId}`, { dataPagamento });
   return response.data;
 };
 
@@ -31,13 +40,17 @@ export const deletePaybleService = async (id: number): Promise<void> => {
   await api.delete(`/payables?id=${id}`);
 };
 
-export const searchPayblesByPeriodService = async (startDate: string, endDate: string): Promise<PaybleList> => {
+export const searchPayblesByPeriodService = async (
+  startDate: string,
+  endDate: string
+): Promise<PaybleList> => {
   try {
-    const response = await api.get<PaybleList>(`/payables/period?startDate=${startDate}&endDate=${endDate}`);
+    const response = await api.get<PaybleList>(
+      `/payables/period?startDate=${startDate}&endDate=${endDate}`
+    );
     return response.data;
   } catch (error) {
     console.error('[searchPayblesByPeriodService] Error:', error);
     throw new Error('Error fetching paybles by period.');
   }
 };
-
